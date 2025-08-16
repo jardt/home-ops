7fe382c8fb68a57e04813dc01305d0f45f65cc6b
Jardar <43233498+jardt@users.noreply.github.com>
2025-08-16 09:45:27 +0200

    Bootstrap cluster
    bootstrapping test

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    python3
+    makejinja
+    talhelper
+    cilium-cli
+    cloudflared
+    cue
+    age
+    fluxcd
+    sops
+    go-task
+    kubernetes-helm
+    helmfile
+    jq
+    kustomize
+    kubectl
+    yq
+    talosctl
+    kubeconform
+    (pkgs.wrapHelm pkgs.kubernetes-helm { plugins = [ pkgs.kubernetes-helmPlugins.helm-diff ]; })
+    minijinja
+  ];
+
+  shellHook = ''
+    python3 -m venv .venv
+    source .venv/bin/activate
+    export KUBECONFIG="$PWD/kubeconfig"
+    export SOPS_AGE_KEY_FILE="$PWD/age.key"
+    export TALOSCONFIG="$PWD/talos/clusterconfig/talosconfig"
+  '';
+}
