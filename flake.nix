c4da4858ac2e9c039aad8055817de868971abf9e
jdr <jdr@internal>
2025-12-11 20:39:40 +0100

    try restore db from dump

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
               export KUBECONFIG="$PWD/kubeconfig"
               export SOPS_AGE_KEY_FILE="$PWD/age.key"
               export TALOSCONFIG="$PWD/talos/clusterconfig/talosconfig"
+              export database=pgcli -h 192.168.1.36 -p 5432 -u postgres -W
             '';
 
           };
